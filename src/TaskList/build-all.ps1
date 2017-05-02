8c6f68e738b8c87389e1742900e9239dbef1868a
daxnet <daxnet@outlook.com>
2017-05-02 22:13:59 +0800

    Fixed the issues in the build script.

--- src/TaskList/build-all.ps1
+++ src/TaskList/build-all.ps1
@@ -6,7 +6,7 @@ $workspace = ($PSScriptRoot).ToString()
 if (Test-Path -Path $workspace\publish) {
 	Remove-Item -Recurse -Force $workspace\publish
 }
-
+dotnet restore -s https://api.nuget.org/v3/index.json -s https://www.myget.org/F/daxnet-utils/api/v3/index.json
 dotnet publish -c Release -o publish
 
 if (!(Test-Path -Path $workspace\publish\wwwroot)) {
@@ -14,4 +14,5 @@ if (!(Test-Path -Path $workspace\publish\wwwroot)) {
 }
 
 cd $workspace\client
+npm install
 ng build -e prod -op $workspace\publish\wwwroot
