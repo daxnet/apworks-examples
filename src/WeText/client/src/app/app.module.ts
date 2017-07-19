7004565df756d2b99208504272bce1d1cf6b1775
daxnet <daxnet@outlook.com>
2017-07-19 20:27:45 +0800

    Added the Login component to the client application.

--- src/WeText/client/src/app/app.module.ts
+++ src/WeText/client/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: LoginComponent
+      }
+    ], { useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
